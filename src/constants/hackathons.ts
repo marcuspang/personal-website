bf0964c4d9cae1deac972aef32afc732a7fa9dfd
marcuspang <marcuspangyuyang@gmail.com>
2023-08-22 08:59:30 +0000

    feat: add hackathon links

--- src/constants/hackathons.ts
+++ src/constants/hackathons.ts
@@ -11,41 +11,49 @@ export const hackathons: Hackathon[] = [
 	{
 		projectTitle: 'Account Abstraction Builder',
 		hackathonTitle: 'ETHTokyo 2023',
-		githubLink: 'https://github.com/marcuspang/account-abstraction-builder'
+		githubLink: 'https://github.com/marcuspang/account-abstraction-builder',
+		websiteLink: 'https://account-abstraction-builder.vercel.app'
 	},
 	{
 		projectTitle: 'DevFi',
 		hackathonTitle: 'Solana Grizzlython 2023',
-		githubLink: 'https://github.com/solana-devfi'
+		githubLink: 'https://github.com/solana-devfi',
+		websiteLink: 'https://devfi-protocol.netlify.app'
 	},
 	{
 		projectTitle: 'BountiFi',
 		hackathonTitle: 'dotGlobal Polkadot Hackathon 2023',
-		githubLink: 'https://github.com/polkadot-bountifi'
+		githubLink: 'https://github.com/polkadot-bountifi',
+		websiteLink: 'https://bountifi.netlify.app'
 	},
 	{
 		projectTitle: 'Alfred Protocol',
 		hackathonTitle: 'Polygon @ the Pit 2023',
-		githubLink: 'https://github.com/Alfred-Protocol'
+		githubLink: 'https://github.com/Alfred-Protocol',
+		websiteLink: 'https://polygon-savings-rate.netlify.app'
 	},
 	{
 		projectTitle: 'DeCoop',
 		hackathonTitle: 'Crypto.com Hackathon 2022',
-		githubLink: 'https://github.com/marcuspang/decoop'
+		githubLink: 'https://github.com/marcuspang/decoop',
+		websiteLink: 'https://decoop.vercel.app'
 	},
 	{
 		projectTitle: 'wentoken',
 		hackathonTitle: 'NUS FinTech Hackathon 2022',
-		githubLink: 'https://github.com/marcuspang/wentoken'
+		githubLink: 'https://github.com/marcuspang/wentoken',
+		websiteLink: 'https://wentoken.vercel.app'
 	},
 	{
 		projectTitle: '101-Daomations',
 		hackathonTitle: 'Hack and Roll 2022',
-		githubLink: 'https://github.com/tanyonghe/101-DAOmatians'
+		githubLink: 'https://github.com/tanyonghe/101-DAOmatians',
+		websiteLink: 'https://101-daomatians.vercel.app'
 	},
 	{
 		projectTitle: 'Learn Together',
 		hackathonTitle: 'LifeHack 2021',
-		githubLink: 'https://github.com/mazx4960/learntogether'
+		githubLink: 'https://github.com/mazx4960/learntogether',
+		websiteLink: 'https://learntogether2021.herokuapp.com'
 	}
 ];
