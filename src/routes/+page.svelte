bcb72664c0cfc6fa348d99d78e0e788230bb0903
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-01-08 06:32:36 +0800

    feat: improve link view on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,16 +26,16 @@
 </section>
 <section class="py-8">
 	<h2 class="font-semibold text-lg text-primary pb-2" id="links">Links</h2>
-	<div class="grid grid-cols-3 gap-y-4">
+	<div class="grid grid-cols-1 sm:grid-cols-3 sm:gap-y-4">
 		{#each links as link}
 			<div class="col-span-1 text-muted-foreground">{link.platform}</div>
-			<div class="col-span-2 text-secondary-foreground">
+			<div class="col-span-2 text-secondary-foreground sm:pb-0 pb-2">
 				{#if link.isExternal}
 					<a
 						href={link.href}
 						target="_blank"
 						rel="noopener"
-						class="dark:hover:text-slate-200 dark:text-slate-300 hover:text-slate-500 text-slate-600 transition-colors"
+						class="dark:hover:text-slate-200 dark:text-slate-300 hover:text-slate-500 text-slate-600 transition-colors break-all"
 					>
 						{link.title}
 						<ExternalLinkIcon class="h-4 w-4 mb-0.5 inline" />
@@ -43,7 +43,7 @@
 				{:else}
 					<a
 						href={link.href}
-						class="dark:hover:text-slate-200 dark:text-slate-300 hover:text-slate-500 text-slate-600 transition-colors"
+						class="dark:hover:text-slate-200 dark:text-slate-300 hover:text-slate-500 text-slate-600 transition-colors break-all"
 					>
 						{link.title}
 					</a>
